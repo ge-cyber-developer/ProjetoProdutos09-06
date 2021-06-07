53a7a93092eafb08f2e5b6a1bc0bf0f3b7bae5d5
Gustavo <gumiguel8@hotmail.com>
2021-06-07 19:18:10 -0300

    métodos e atributos criados

--- Classes/Usuario.cs
+++ Classes/Usuario.cs
@@ -1,7 +1,22 @@
+using System;
+
 namespace ProjetoProduto09_06.Classes
 {
     public class Usuario
     {
-        
+        private int Codigo;
+        private string Nome;
+        private string Email;
+        private string Senha;
+        private DateTime DataCadastro;
+
+        public string Cadastrar(Usuario usuario){
+            return "bla";
+        }
+
+        public string Deletar(Usuario usuario){
+            return "bla";
+        }
+
     }
 }
