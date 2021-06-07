f4d39b00f96e3cbeb9916e23e13efa42b16a46d6
Gustavo <gumiguel8@hotmail.com>
2021-06-07 16:22:45 -0300

    teste de branches

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ProjetoProduto09_06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("teste");
         }
     }
 }
