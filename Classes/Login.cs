f327a65792e21a8b576f96f4b5b8c90ae19c8ca0
Gustavo <gumiguel8@hotmail.com>
2021-06-07 17:38:25 -0300

    métodos e atributos criados

--- Classes/Login.cs
+++ Classes/Login.cs
@@ -1,7 +1,18 @@
+using System;
+
 namespace ProjetoProduto09_06.Classes
 {
     public class Login
     {
-        
+        private bool Logado;
+        public Login(){
+            Console.WriteLine("Menu");
+        }
+        public string Logar(Usuario usuario){
+            return $"Usuario {usuario} logado";
+        }
+        public string Deslogar(Usuario usuario){
+            return $"Usuario {usuario} deslogado";
+        }
     }
 }
